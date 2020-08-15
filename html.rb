2371a1ea5d40806be7f57ea17febc2801baefddb
slapstop0320 <slapstop0320@gmail.com>
2020-08-15 16:47:58 +0900

    テーブルヘッダの固定

--- html.rb
+++ html.rb
@@ -9,11 +9,12 @@ print <<EOS
     <link rel="stylesheet" href="average.css">
   </head>
   <body>
-    <table>
-      <thead>
-        <tr><th>区分1</th><th>区分2</th><th>コース</th><th>科目名</th><th>2019年度2学期</th><th>2019年度1学期</th></tr>
-      </thead>
-      <tbody>
+    <div class="sticky_table_wrapper" height="100%">
+      <table class="sticky_table">
+        <thead>
+          <tr><th>区分1</th><th>区分2</th><th>コース</th><th>科目名</th><th>2019年度2学期</th><th>2019年度1学期</th></tr>
+        </thead>
+        <tbody>
 EOS
 
 averages = AVERAGES.sort_by do |x|
@@ -33,13 +34,14 @@ averages.each do |average|
     difficulty1 = ave1.to_f >= 90 ? ' class="very_easy"' : (ave1.to_f < 60 ? ' class="hard"' : '')
   end
   print <<-EOS
-        <tr><td>#{average[3]}</td><td>#{average[4]}</td><td>#{average[5]}</td><td>#{average[0]}</td><td#{difficulty2}>#{ave2}</td><td#{difficulty1}>#{ave1}</td></tr>
+          <tr><td>#{average[3]}</td><td>#{average[4]}</td><td>#{average[5]}</td><td>#{average[0]}</td><td#{difficulty2}>#{ave2}</td><td#{difficulty1}>#{ave1}</td></tr>
   EOS
 end
 
 print <<EOS
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </div>
   </body>
 </html>
 EOS
