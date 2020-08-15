7030a82e843acd3f24763ca339c8c0355140e58d
slapstop0320 <slapstop0320@gmail.com>
2020-08-15 12:23:54 +0900

    コメントアウトしたコードを削除

--- html.rb
+++ html.rb
@@ -19,17 +19,7 @@ EOS
 averages = AVERAGES.sort_by do |x|
   /2019年度2学期（(?<ave2>[0-9.]+)点）/ =~ x[1]
   /2019年度1学期（(?<ave1>[0-9.]+)点）/ =~ x[2]
-  [x[5], x[4], x[3], ave2.to_f * -1, ave1.to_f * -1, x[0]] #, x[1].to_f, x[2].to_f, x[0]]
-#   cmp = [a[0] <=> b[0], a[1].to_f <=> b[1].to_f, a[2].to_f <=> b[2].to_f, a[3] <=> b[3], a[4] <=> b[4], a[5] <=> b[5]]
-#   
-#   cmp[1]
-#   if cmp[3] != 0 then cmp[3] * -1
-#   elsif cmp[4] != 0 then cmp[4]
-#   elsif cmp[5] != 0 then cmp[5]
-#   elsif cmp[1] != 0 then cmp[1]
-#   elsif cmp[2] != 0 then cmp[2]
-#   else cmp[0]
-#   end
+  [x[5], x[4], x[3], ave2.to_f * -1, ave1.to_f * -1, x[0]]
 end
 
 averages.each do |average|
