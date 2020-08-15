b3e9ecf0628dfb4a4e5c7bd3f25472710b32043e
slapstop0320 <slapstop0320@gmail.com>
2020-08-15 09:53:46 +0900

    first commit

---
+++ html.rb
@@ -0,0 +1,39 @@
+require_relative 'average'
+
+print <<EOS
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>放送大学　単位認定試験　平均点</title>
+    <link rel="stylesheet" href="average.css">
+  </head>
+  <body>
+    <table>
+      <thead>
+        <tr><th>科目名</th><th>2019年度2学期</th><th>2019年度1学期</th></tr>
+      </thead>
+      <tbody>
+EOS
+
+AVERAGES.each do |average|
+  ave2 = ave1 = nil
+  /2019年度2学期（(?<ave2>[0-9.]+)点）/ =~ average[1]
+  /2019年度1学期（(?<ave1>[0-9.]+)点）/ =~ average[2]
+  if !ave2.nil? && !ave2.empty?
+    difficulty2 = ave2.to_f >= 90 ? ' class="very_easy"' : (ave2.to_f < 60 ? ' class="hard"' : '')
+  end
+  if !ave1.nil? && !ave1.empty?
+    difficulty1 = ave1.to_f >= 90 ? ' class="very_easy"' : (ave1.to_f < 60 ? ' class="hard"' : '')
+  end
+  print <<-EOS
+        <tr><td>#{average[0]}</td><td#{difficulty2}>#{ave2}</td><td#{difficulty1}>#{ave1}</td></tr>
+  EOS
+end
+
+print <<EOS
+      </tbody>
+    </table>
+  </body>
+</html>
+EOS
